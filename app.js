1c0df6cfb37f5c8a89e8b640c99fdb2cbb8d4c94
geethanjalig56 <g.gayan9451@gmail.com>
2024-02-09 14:50:13 +0530

    completed adding post

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const blogrouter = require("./controller/blogRoute")
-
+const postRouter = require("./controller/postRouter")
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/api/blog",blogrouter)
+app.use("/api/post",postRouter)
+
 mongoose.connect("mongodb+srv://geethanjali2001:ammu2001@cluster0.iwq8qez.mongodb.net/blogDb?retryWrites=true&w=majority",{useNewUrlParser:true})
 app.listen(3002, ()=>{
     console.log("yep server running")
